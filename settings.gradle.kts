543043d84cae9deb2210a3c7ce12e1104abd3592
qq1322798305 <1322798305@qq.com>
2024-06-26 23:13:34 +0800

    修正settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,8 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
-        maven("https://jitpack.io")
+        google()
+        mavenCentral()
     }
 }
 dependencyResolutionManagement {
@@ -17,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
