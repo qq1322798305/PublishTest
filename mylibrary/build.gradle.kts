7c052883f647ad89c0b683ce45ccea717e4cf7be
xcc <xiaocydx@163.com>
2024-06-27 10:48:24 +0800

    feat: 补充回配置

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -4,6 +4,15 @@ plugins {
     id("maven-publish")
 }
 
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.qq1322798305"
+            afterEvaluate { from(components["release"]) }
+        }
+    }
+}
+
 android {
     namespace = "com.example.mylibrary"
     compileSdk = 34
